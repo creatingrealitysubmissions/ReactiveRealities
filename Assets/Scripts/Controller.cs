762d408e7d16858c367d9f3856a357532c68f80c
Kaan Demiroz <demiroz@usc.edu>
2018-03-13 20:11:28 -0700

    Controller / Stretch interaction started

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.WSA.Input;
+
+public class Controller : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InteractionSourceState[] interactionSourceStates = InteractionManager.GetCurrentReading();
+
+        bool leftPressed = interactionSourceStates.Length > 0 && interactionSourceStates[0].selectPressed;//Input.GetAxis("10") == 1);
+        bool rightPressed = interactionSourceStates.Length > 1 && interactionSourceStates[1].selectPressed;// Input.GetAxis("9") == 1);
+
+        if (leftPressed && rightPressed)
+        {
+
+        }
+        else
+        {
+            if (leftPressed)
+            {
+                Debug.Log("Right trigger pressed");
+                Vector3 rightHandPosition = InputTracking.GetLocalPosition(XRNode.RightHand);
+                Debug.DrawLine(rightHandPosition, rightHandPosition * 2);
+            }
+            if (rightPressed)
+            {
+                Debug.Log("Left trigger pressed");
+                Vector3 leftHandPosition = InputTracking.GetLocalPosition(XRNode.LeftHand);
+                Debug.DrawLine(leftHandPosition, leftHandPosition * 2);
+            }
+        }
+    }
+}
