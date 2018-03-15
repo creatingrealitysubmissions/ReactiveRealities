fb8db3b939ced9fbaa2422d4d0084beca4d0d27b
Kaan Demiroz <demiroz@usc.edu>
2018-03-14 17:01:44 -0700

    Level 0

---
+++ Assets/Scripts/ChopTree.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChopTree : MonoBehaviour
+{
+    [SerializeField] GameObject[] trees;
+
+    int currentTreeIndex;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentTreeIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (currentTreeIndex < trees.Length)
+        {
+            trees[currentTreeIndex].SetActive(false);
+            currentTreeIndex++;
+            if (currentTreeIndex < trees.Length)
+                trees[currentTreeIndex].SetActive(true);
+        }
+    }
+}
